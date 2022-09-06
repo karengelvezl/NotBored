ee7dab09cd027beae59ed580bb955e780fc40f6d
pabcarballo <pablojavier.carballo@mercadolibre.com>
2022-09-05 23:21:39 -0300

    -Add dependencies -Add in main : Toolbar - FragmentConteiner -Add nav_graph.xml

--- app/src/main/java/com/example/notbored/ui/MainActivity.kt
+++ app/src/main/java/com/example/notbored/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.notbored
+package com.example.notbored.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notbored.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
