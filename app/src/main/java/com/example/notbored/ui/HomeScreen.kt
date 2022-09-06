b488631f274c4146c70d2473096c40fd5767a26f
pabcarballo <pablojavier.carballo@mercadolibre.com>
2022-09-06 16:49:05 -0300

    .asd

---
+++ app/src/main/java/com/example/notbored/ui/HomeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.notbored.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.notbored.R
+import com.example.notbored.databinding.FragmentHomeScreenBinding
+
+
+class HomeScreen : Fragment() {
+    private lateinit var binding: FragmentHomeScreenBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentHomeScreenBinding.bind(view)
+
+    }
+
+
+
+
+}
