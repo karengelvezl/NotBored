f5ea0aae5959156c9d5d4e333b5aa6a7581b0abd
pabcarballo <pablojavier.carballo@mercadolibre.com>
2022-09-08 17:25:37 -0300

    -Add activity main

--- app/src/main/java/com/example/notbored/ui/MainActivity.kt
+++ app/src/main/java/com/example/notbored/ui/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.notbored.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.notbored.R
+import com.example.notbored.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        sendActivitiesScreen()
+        sendTermsAndConditions()
+    }
+
+    private fun sendActivitiesScreen() {
+        binding.btnStart.setOnClickListener {
+            val participants = binding.numberParticipants
+            val numberParticipants = participants.toString().toInt()
+            val intent = Intent(this, ActivitiesScreen::class.java).apply {
+                putExtra("numberParticipants", numberParticipants)
+            }
+            startActivity(intent)
+        }
+
+
+    }
+
+    private fun sendTermsAndConditions() {
+        binding.txtTermsAndConditions.setOnClickListener {
+            val intent = Intent(this, TermsAndConditionsActivity::class.java).apply {
+            }
+            startActivity(intent)
+        }
+
+
     }
 }
