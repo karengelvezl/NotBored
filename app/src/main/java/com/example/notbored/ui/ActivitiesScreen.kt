7898a270d5123beca56022b132ff4d63111f23a6
karengelvezl <karen.gelvez@mercadolibre.com.co>
2022-09-09 05:56:10 -0500

    Show activity in SuggestionScreen

--- app/src/main/java/com/example/notbored/ui/ActivitiesScreen.kt
+++ app/src/main/java/com/example/notbored/ui/ActivitiesScreen.kt
@@ -2,15 +2,14 @@ package com.example.notbored.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.notbored.R
 import com.example.notbored.databinding.ActivityActivitiesScreenBinding
 
 class ActivitiesScreen : AppCompatActivity() {
 
-    private lateinit var  binding: ActivityActivitiesScreenBinding
-    private lateinit var adapter: ActivitiesAdapter
+    private lateinit var binding: ActivityActivitiesScreenBinding
+    private lateinit var numberParticipants: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +18,7 @@ class ActivitiesScreen : AppCompatActivity() {
 
         val activities = resources.getStringArray(R.array.activities).toList()
         binding.lvActivities.adapter = ActivitiesAdapter(this, activities)
-        var numberParticipants: String = ""
+        numberParticipants = ""
         val bundle: Bundle? = intent.extras
 
         bundle?.let {
@@ -28,21 +27,21 @@ class ActivitiesScreen : AppCompatActivity() {
                 numberParticipants = getString("numberParticipants").toString()
             }
         }
-
-        val intent = Intent(this, SuggestionScreen::class.java)
-        with(binding){
+        numberParticipants
+        val intent2 = Intent(this, SuggestionScreen::class.java)
+        with(binding) {
             lvActivities.setOnItemClickListener { adapterView, view, i, l ->
                 val typeActivity = adapterView.getItemAtPosition(i) as String
-                //llamada a la API con el putInt y el tipo de actividad numberParticipants -> true
-                intent.putExtra("numberParticipants", numberParticipants)
-                intent.putExtra("nameActivity", typeActivity.lowercase())
-                intent.putExtra("random", false)
-                startActivity(intent)
+                //llamada a la API con el putInt y el tipo de actividad numberParticipants
+                intent2.putExtra("numberParticipants", numberParticipants)
+                intent2.putExtra("nameActivity", typeActivity.lowercase())
+                intent2.putExtra("random", false)
+                startActivity(intent2)
             }
             shuffle.setOnClickListener {
-                //llamar al metodo random de la api -> false
-                intent.putExtra("random", true)
-                startActivity(intent)
+                //llamar al metodo random de la api
+                intent2.putExtra("random", true)
+                startActivity(intent2)
             }
         }
     }
--- app/src/main/java/com/example/notbored/ui/service/responseObjects/SuggestionResponse.kt
+++ app/src/main/java/com/example/notbored/ui/service/responseObjects/SuggestionResponse.kt
@@ -3,8 +3,10 @@ package com.example.notbored.ui.service.responseObjects
 data class SuggestionResponse(
     var activity: String,
     var type: String,
-    var participants: Int,
-    var price: Int,
-    var link: String,
+    var participants: String,
+    var price: String,
+    var link: String? = "",
     var key: String,
-    var accessibility: Int)
+    var accessibility: String,
+    var error: String? = ""
+)
